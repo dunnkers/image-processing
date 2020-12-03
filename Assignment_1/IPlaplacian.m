30ceaa768b0232cef43b3e88cbf91ccb3f5f5cb7
Jeroen Overschie <jeroenoverschie@gmail.com>
2020-12-03 12:30:49 +0100

    Slight code refactor

--- Assignment_1/IPlaplacian.m
+++ Assignment_1/IPlaplacian.m
@@ -2,22 +2,17 @@
 % It takes the orginal image and a chosen kernel as input
 % It outputs the enhanced image
 function result = IPlaplacian(f, kernel)
-    [M, N] = size(f); %M = height, N = width
     laplacian = IPfilter(f, kernel); % create laplacian image
 
-    % substract laplacian image from orginal image
-    result = zeros(M,N);
-    if (all(all(kernel == [0,1,0; 1,-4,1; 0,1,0])) || ...
-        all(all(kernel == [1,1,1; 1,-8,1; 1,1,1])))
+    % substract or add laplacian image from orginal image,
+    % depending on kernel used. See book Figure 3.45, section 3.6.
+    if (isequal(kernel, [0,1,0; 1,-4,1; 0,1,0]) || ...
+        isequal(kernel, [1,1,1; 1,-8,1; 1,1,1]))
         c = -1;
     else
         c = 1;
     end
     
-    for x=1:M
-        for y=1:N
-            result(x,y) = f(x,y) + c * laplacian(x,y);
-        end
-    end
+    result = f + c * laplacian; % add filter to image
 end
 
