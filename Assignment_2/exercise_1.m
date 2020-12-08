4e526af1aefe9ac5cbecca9ed9c1486cdc9a6e56
KevinGevers <kevin_gevers@hotmail.com>
2020-12-08 12:08:47 +0100

    progress on lab 2 exercise 1

--- Assignment_2/exercise_1.m
+++ Assignment_2/exercise_1.m
@@ -1,11 +1,25 @@
 clc;
-imname = 'characters';
+imname = 'circuitboard';
 inputfile = ['input_images/', imname,'.tif'];          
 f = imread(inputfile);
+f = im2double(f);
 
 % compute its centered Fourier spectrum
-fft2() %fast fourier transform 2D
-fftshift() %Shift zero-frequency component to center of spectrum
+DFT = fft2(f); %fast fourier transform 2D
+
+% spectrum = sqrt(real(DFT)^2 + imag(DFT)^2);
+shiftedDFT = fftshift(F); %Shift zero-frequency component to center of spectrum
+spectrumShifted = sqrt(real(shiftedDFT)^2 + imag(shiftedDFT)^2);
+
+figure;
+plot(abs(DFT))
+figure;
+plot(abs(shiftedDFT))
+figure;
+plot(abs(spectrumShifted))
+
+
+
 
 % Display the spectrum.
 
