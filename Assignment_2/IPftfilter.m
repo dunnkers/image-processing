8e400a2779fac5d344be7b7690db6d45216608b1
Jeroen Overschie <jeroenoverschie@gmail.com>
2020-12-10 15:04:42 +0100

    Remove summary steps
    We no longer follow the book summary 1:1 anyway.

--- Assignment_2/IPftfilter.m
+++ Assignment_2/IPftfilter.m
@@ -1,7 +1,7 @@
 function g = IPftfilter(x, H)
-% IPftfilter Perform convolution filtering. Following Eq (4-104) and steps
-% as defined in Section 4.7 'Summary of steps for filtering in the
-% frequency domain'.
+% IPftfilter Perform convolution filtering. Partially following Eq (4-104) 
+% and steps as defined in Section 4.7 'Summary of steps for filtering in 
+% the frequency domain'.
 %   Arguments:
 %       x = input image
 %       H = transfer function
@@ -9,18 +9,18 @@ function g = IPftfilter(x, H)
 %       g = filtered image
 [M, N] = size(x); % height, width
 
-% Determine padding sizes (1)
+% Determine padding sizes
 P = 2*M; % Eq (4-102)
 Q = 2*N; % Eq (4-103)
 
-% Form a padded image using zero-padding (2)
+% Form a padded image using zero-padding
 f_p = zeros(P, Q);
 f_p(1:M, 1:N) = im2double(x); % copy over original image
 
-% Compute DFT (4). 
+% Compute DFT
 F = fftshift(fft2(f_p)); % Eq (4-67)
 
-% Form the product G(u, v) (6)
+% Form the product G(u, v)
 G = F .* H; % Eq (4-104)
 
 % Obtain filtered image, compute inverse DFT of G(u, v)
