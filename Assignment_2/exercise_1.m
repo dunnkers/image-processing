68de8433c36d7ccb7254182715c9f92ace8f322e
KevinGevers <kevin_gevers@hotmail.com>
2020-12-10 11:54:33 +0100

    update exercise 1 lab 2

--- Assignment_2/exercise_1.m
+++ Assignment_2/exercise_1.m
@@ -1,80 +1,40 @@
 clc;
+clear;
+close all;
+
 imname = 'characters';
 inputfile = ['input_images/', imname,'.tif'];          
 f = imread(inputfile);
+f = im2double(f);
 
-figure;
-subplot(131)
-imshow(abs(fft2(f)),[])
-subplot(132)
-imshow(log(abs(fft2(f))),[])
-subplot(133)
-imshow(log(abs(fftshift(fft2(f)))),[])
+im1 = abs(fft2(f));
+im2 = abs(fftshift(fft2(f)));
+im3 = log(abs(fft2(f)));
+im4 = log(abs(fftshift(fft2(f))));
 
-% % f = im2double(f);
-% x = f;
-% 
-% % compute its centered Fourier spectrum
-% [M, N] = size(x); % height, width
-% 
-% % Determine padding sizes (1)
-% P = 2*M; % Eq (4-102)
-% Q = 2*N; % Eq (4-103)
-% 
-% % Form a padded image using zero-padding (2)
-% f_p = zeros(P, Q);
-% f_p(1:M, 1:N) = im2double(x); % copy over original image
-% 
-% % Center the Fourier Transform (3)
-% [X, Y] = meshgrid(1:P, 1:Q);
-% % % c = -1^(X + Y); % centering term, Eq (4-76)
-% f_c = f_p * c;  % multiply by $-1^(x + y)$
-% 
-% % Compute DFT (4). 
-% F = fftshift(fft2(f_p)); % Eq (4-67)
+% DFT = fftshift(fft2(f));
+% spectrum = sqrt(real(DFT).^2 + imag(DFT).^2);
+% im4 = log(spectrum);
 
+mean(fftshift(fft2(f)), 'all')
+mean(f, 'all')
 
-% 
-% 
-% 
-% [M, N] = size(f); % height, width
-% 
-% % Form a padded image using zero-padding (2)
-% f_p = zeros(M, N);
-% f_p(1:M, 1:N) = f; % copy over original image
-% 
-% % Center the Fourier Transform (3)
-% [X, Y] = meshgrid(1:M, 1:N);
-% c = -1^(X + Y); % centering term, Eq (4-76)
-% f_c = f_p * c;  % multiply by $-1^(x + y)$
-% 
-% % Compute DFT (4). 
-% F = fftshift(fft2(f_c)); % Eq (4-67)
-
-
-
-% % Center the Fourier Transform (3)
-% [M, N] = size(f);
-% [X, Y] = meshgrid(1:M, 1:N);
-% c = -1^(X + Y); % centering term, Eq (4-76)
-% f_c = f_p * c;  % multiply by $-1^(x + y)$
-% 
-% DFT = fft2(f_c); %fast fourier transform 2D
-% 
-% % spectrum = sqrt(real(DFT)^2 + imag(DFT)^2);
-% shiftedDFT = fftshift(DFT); %Shift zero-frequency component to center of spectrum
-% % spectrumShifted = sqrt(real(shiftedDFT)^2 + imag(shiftedDFT)^2);
-% 
-% figure;
-% plot(abs(DFT))
-% figure;
-% plot(abs(shiftedDFT))
-% figure;
-% plot(abs(spectrumShifted))
-% 
-
-
+temp = fft2(f);
+temp(1,1)
 
 % Display the spectrum.
+figure;
+subplot(221);
+imshow(im1,[])
+title('fft')
+subplot(222);
+imshow(im2,[])
+title('fftshift + fft')
+subplot(223);
+imshow(im3,[])
+title('abs of fft')
+subplot(224);
+imshow(im4,[])
+title('abs of fftshift + fft')
 
 % Use your result in (a) to compute the average value of the image.
