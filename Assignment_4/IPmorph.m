c48f133fd76fd17e8dccc932eeee5f154b868b27
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-01-14 12:16:23 +0100

    Better IPmorph comments

--- Assignment_4/IPmorph.m
+++ Assignment_4/IPmorph.m
@@ -1,11 +1,14 @@
 function Imorph = IPmorph(I, B, type)
 % IPmorph Applies a morphological transformation to a binary image using 
-%         some structuring element.
+%         some structuring element. It is assumed the SE origin is at its
+%         center.
 %   Arguments:
 %       I: input image to transform. Must be binary, i.e. have logical values.
 %       B: structuring element (SE). Must have odd dimensions and also be 
 %       binary, i.e. have logical values. Its origin is automatically set 
 %       at its centerpoint.
+%       type: morphological transformation type. Can be either 'erode' or
+%       'dilate'.
 %   Returns: morphologically transformed image
 
 % Dimensions
@@ -17,15 +20,17 @@ assert(islogical(B));
 % Ensure SE has odd dimensions
 assert(rem(Mse, 2) ~= 0)
 assert(rem(Nse, 2) ~= 0)
+% Ensure type argument is of acceptable value
+assert(ismember(type, {'erode', 'dilate'}));
 
 % Put padding around original image. Then, SE can freely move around.
 pad_y = floor(Mse / 2);
 pad_x = floor(Nse / 2);
 switch type
-    case 'dilate'
-        Ipad = padarray(I, [pad_y, pad_x], 0, 'both');
-    case 'erode'
+    case 'erode'    % pad with 1's in case of erosion
         Ipad = padarray(I, [pad_y, pad_x], 1, 'both');
+    case 'dilate'   % pad with 0's in case of dilation
+        Ipad = padarray(I, [pad_y, pad_x], 0, 'both');
 end
 
 % Loop image pixels
@@ -37,10 +42,10 @@ for y=(1:M)+pad_y
         A = Ipad(SE_ycoords, SE_xcoords);
         
         switch type
-            case 'dilate'
-                value = any(A(B));
-            case 'erode'
+            case 'erode'  % erode:  1 iff SE 'fits' neighborhood exactly.
                 value = all(A(B));
+            case 'dilate' % dilate: 1 iff SE 'hits' any in neighboorhood.
+                value = any(A(B));
         end
         Imorph(y - pad_y, x - pad_x) = value;
     end 
